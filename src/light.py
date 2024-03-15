e06c93d1c77b6fe9162657748b66b784eb858595
forLinDre <andrey9312@gmail.com>
2024-03-15 14:50:57 -0700

    Streamlit experiment

--- src/light.py
+++ src/light.py
@@ -4,6 +4,10 @@ from pi.on_off_relay import OFRelay
 
 class Light(OFRelay):
     def __init__(self, pin, active_high=True, initial_value=False):
-        OFRelay.__init__(pin=pin, active_high=active_high, initial_value=initial_value)
+        OFRelay.__init__(
+            self, pin=pin,
+            active_high=active_high,
+            initial_value=initial_value
+        )
 
 
